c19b182c9f8a18edf1c3987a0b4aaf54deae9b77
Yunqing Wang <yunqing.wang@tum.de>
2022-10-16 18:45:58 +0200

    Exercise-Template pushed by Artemis

---
+++ src/de/tum/in/ase/Shopping.java
@@ -0,0 +1,27 @@
+package de.tum.in.ase;
+
+
+public final class Shopping {
+	private final Item[] shoppingList;
+	private int bagCapacity;
+
+	public Shopping(Item[] shoppingList, int bagCapacity) {
+		this.shoppingList = shoppingList;
+		this.bagCapacity = bagCapacity;
+	}
+
+	// TODO: implement search(String itemName)
+
+	// TODO: implement findMin()
+
+	// TODO: implement findMax()
+
+	// TODO: implement fillBagMax()
+
+	// TODO: implement calValue()
+
+
+	public static void main(String[] args) {
+		//you can test your code here by creating your own shopping object
+	}
+}
